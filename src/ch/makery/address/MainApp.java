dd33a98262dae485394763c1204b6b152e003351
Po-Sheng Chang <posheng.chang@insyde.com>
2016-07-15 13:31:45 +0800

    Add user edit dialog and css feature
    1. Add a dialog stage for editing user information
    2. Using CSS to style user interface

--- src/ch/makery/address/MainApp.java
+++ src/ch/makery/address/MainApp.java
@@ -3,14 +3,17 @@ package ch.makery.address;
 import java.io.IOException;
 
 import ch.makery.address.model.Person;
+import ch.makery.address.view.PersonEditDialogController;
 import ch.makery.address.view.PersonOverviewController;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
@@ -40,6 +43,7 @@ public class MainApp extends Application {
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle("AddressApp");
+		this.primaryStage.getIcons().add(new Image("file:resources/images/address_book_32.png"));
 
 		initRootLayout();
 		showPersonOverview();
@@ -74,6 +78,39 @@ public class MainApp extends Application {
 			e.printStackTrace();
 		}
 
+	}
+
+	public boolean showPersonEditDialog(Person person) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/PersonEditDialog.fxml"));
+			AnchorPane page = (AnchorPane) loader.load();
+
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Edit Person");
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.initOwner(primaryStage);
+
+			Scene scene = new Scene(page);
+			dialogStage.setScene(scene);
+
+			PersonEditDialogController controller = loader.getController();
+			controller.setDialogStage(dialogStage);
+			controller.setPerson(person);
+
+			dialogStage.showAndWait();
+
+			return controller.isOkClicked();
+		} catch (IOException e) {
+			e.printStackTrace();
+
+			return false;
+		}
+
+	}
+
+	public Stage getPrimaryStage() {
+		return this.primaryStage;
 	}
 
 	public static void main(String[] args) {
--- src/ch/makery/address/view/PersonOverviewController.java
+++ src/ch/makery/address/view/PersonOverviewController.java
@@ -2,7 +2,10 @@ package ch.makery.address.view;
 
 import ch.makery.address.MainApp;
 import ch.makery.address.model.Person;
+import ch.makery.address.util.DateUtil;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -45,11 +48,83 @@ public class PersonOverviewController {
 	private void initialize() {
 		this.firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
 		this.lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+
+		showPersonDetails(null);
+
+		personTable.getSelectionModel().selectedItemProperty()
+				.addListener((observable, oldValue, newValue) -> showPersonDetails(newValue));
+
+	}
+
+	@FXML
+	private void handleNewPerson() {
+		Person tempPerson = new Person();
+		boolean onClicked = mainApp.showPersonEditDialog(tempPerson);
+
+		if (onClicked) {
+			mainApp.getPersonData().add(tempPerson);
+		}
+	}
+
+	@FXML
+	private void handleEditPerson() {
+		Person selectedPerson = personTable.getSelectionModel().getSelectedItem();
+
+		if (selectedPerson != null) {
+			boolean okClicked = mainApp.showPersonEditDialog(selectedPerson);
+
+			if (okClicked) {
+				showPersonDetails(selectedPerson);
+			}
+		} else {
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.initOwner(mainApp.getPrimaryStage());
+			alert.setTitle("No Selection");
+			alert.setHeaderText("No Person Selected");
+			alert.setContentText("Please select a person in the table.");
+
+			alert.showAndWait();
+		}
+	}
+
+	@FXML
+	private void handleDeletePerson() {
+		int selectedIndex = personTable.getSelectionModel().getSelectedIndex();
+		if (selectedIndex >= 0) {
+			personTable.getItems().remove(selectedIndex);
+		} else {
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.initOwner(mainApp.getPrimaryStage());
+			alert.setTitle("No Selection");
+			alert.setHeaderText("No Person Selected");
+			alert.setContentText("Please select a person in the table.");
+
+			alert.showAndWait();
+		}
 	}
 
 	public void setMainApp(MainApp mainApp) {
 		this.mainApp = mainApp;
 
-		this.personTable.setItems(this.mainApp.getPersonData());
+		this.personTable.setItems(mainApp.getPersonData());
 	}
+
+	private void showPersonDetails(Person person) {
+		if (person != null) {
+			this.firstNameLabel.setText(person.getFirstName());
+			this.lastNameLabel.setText(person.getLastName());
+			this.streetLabel.setText(person.getStreet());
+			this.postalCodeLabel.setText(Integer.toString(person.getPostalCode()));
+			this.cityLabel.setText(person.getCity());
+			this.birthdayLabel.setText(DateUtil.format(person.getBirthday()));
+		} else {
+			this.firstNameLabel.setText("");
+			this.lastNameLabel.setText("");
+			this.streetLabel.setText("");
+			this.postalCodeLabel.setText("");
+			this.cityLabel.setText("");
+			this.birthdayLabel.setText("");
+		}
+	}
+
 }
